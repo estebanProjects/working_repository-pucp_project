2830d691d2fcc6e5f988732b7fa9c7392d9893c6
Esteban Espinoza <estebanespinoza1905@gmail.com>
2021-07-24 11:20:56 -0500

    Base de los problemas.js(falta direccionar las imagenes a dropbox)

---
+++ src/public/javascript/problemas.js
@@ -0,0 +1,57 @@
+
+let arrayProblemasPucp_Ciencia = [{ // ARITMETICA
+    imgProblema: "./ejercicios/aritmetica/problemas/arit_problema1.jpg",
+    respuesta: "c",
+    imgResolucion: "./ejercicios/aritmetica/resolucion/arit_resolucion1.jpg",
+    curso: 'aritmetica'
+},{
+    imgProblema: "./ejercicios/aritmetica/problemas/arit_problema2.jpg",
+    respuesta: "c",
+    imgResolucion: "./ejercicios/aritmetica/resolucion/arit_resolucion2.jpg",
+    curso: 'aritmetica'
+},{
+    imgProblema: "./ejercicios/aritmetica/problemas/arit_problema3.jpg",
+    respuesta: "c",
+    imgResolucion: "./ejercicios/aritmetica/resolucion/arit_resolucion3.jpg",
+    curso: 'aritmetica'
+},{ // ALGEBRA
+    imgProblema: "./ejercicios/algebra/problemas/alg_problema1.jpg",
+    respuesta: "d",
+    imgResolucion: "./ejercicios/algebra/resolucion/alg_resolucion1.jpg",
+    curso: 'algebra'
+},{
+    imgProblema: "./ejercicios/algebra/problemas/alg_problema2.jpg",
+    respuesta: "b",
+    imgResolucion: "./ejercicios/algebra/resolucion/alg_resolucion2.jpg",
+    curso: 'algebra'
+},{
+    imgProblema: "./ejercicios/algebra/problemas/alg_problema3.jpg",
+    respuesta: "b",
+    imgResolucion: "./ejercicios/algebra/resolucion/alg_resolucion3.jpg",
+    curso: 'algebra'
+},{ // GEOMETRIA
+    imgProblema: "./ejercicios/geometria/problemas/geo_problema1.jpg", // cambiar de ejercicio - resolucion mal hecha
+    respuesta: "d",
+    imgResolucion: "./ejercicios/geometria/resolucion/geo_resolucion1.jpg",
+    curso: 'geometria'
+},{
+    imgProblema: "./ejercicios/geometria/problemas/geo_problema2.jpg", 
+    respuesta: "a",
+    imgResolucion: "./ejercicios/geometria/resolucion/geo_resolucion2.jpg",
+    curso: 'geometria'
+},{
+    imgProblema: "./ejercicios/geometria/problemas/geo_problema3.jpg", 
+    respuesta: "a",
+    imgResolucion: "./ejercicios/geometria/resolucion/geo_resolucion3.jpg",
+    curso: 'geometria'
+}
+]
+
+let arrayProblemasPucp_Letras = [
+    {
+
+    }, 
+    {
+        
+    }
+]
