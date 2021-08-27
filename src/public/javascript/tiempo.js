fcfae217ad7e372de757c342cf5b3a697682446a
Omar <omaralvh0@gmail.com>
2021-08-27 18:27:40 -0500

    cambios pre-pre lanzamiento

--- src/public/javascript/tiempo.js
+++ src/public/javascript/tiempo.js
@@ -53,7 +53,7 @@ function correrTiempo(grado) {
          cargarMinutos(segundos,minutos)}
      } else {
             if(grado=="general"){
-            segundosGeneralHtml.innerHTML = "00";document.getElementsByClassName("generaltime")[0].style.backgroundColor = "darkred";document.getElementsByClassName("generaltime")[0].style.border = "5px darkred";
+            segundosGeneralHtml.innerHTML = "00";document.getElementsByClassName("generaltime")[0].style.backgroundColor = "darkred";document.getElementsByClassName("generaltime")[0].style.borderColor = "red";
             }
             else{segundosParticularHtml.innerHTML = "00";document.getElementsByClassName("time")[0].style.backgroundColor = "red";}
 }
